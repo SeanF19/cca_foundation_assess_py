da24464cc1de2301c3ab84c78ee082a2e63efb76
Sean <sean@kayenta.io>
2024-03-18 16:11:09 +0000

    add warehouse stock

--- src/warehouse.py
+++ src/warehouse.py
@@ -7,8 +7,21 @@ class Entry:
     product: Product
     stock: int
 
+    @property
+    def product_id(self):
+        return self.product.id
 
-@dataclass
+
 class Warehouse:
-    catalogue: list[Entry]
+    catalogue: dict[int, Entry] = {}
 
+    def __init__(self, catalogue: list[Entry]):
+        for entry in catalogue:
+            self.add_stock(entry)
+
+    def add_stock(self, entry: Entry) -> None:
+        if not (catalogue_entry := self.catalogue.get(entry.product_id)):
+            self.catalogue[entry.product_id] = entry
+        else:
+            catalogue_entry.stock += entry.stock
+            self.catalogue[entry.product_id] = catalogue_entry
