7fc088cf035e0604887e0b3055b30f9de8493d10
Sean <sean@kayenta.io>
2024-03-18 14:03:16 +0000

    add product definitions

--- src/product.py
+++ src/product.py
@@ -7,3 +7,39 @@ class Product:
     description: str
     price: float
 
+
+red_guitar = Product(
+    id=1,
+    description="Red Guitar",
+    price=100,
+)
+
+blue_guitar = Product(
+    id=2,
+    description="Blue Guitar",
+    price=70,
+)
+
+green_guitar = Product(
+    id=3,
+    description="Green Guitar",
+    price=85,
+)
+
+yellow_guitar = Product(
+    id=4,
+    description="Yellow Guitar",
+    price=110,
+)
+
+purple_guitar = Product(
+    id=5,
+    description="Purple Guitar",
+    price=120,
+)
+
+black_guitar = Product(
+    id=6,
+    description="Black Guitar",
+    price=60,
+)
